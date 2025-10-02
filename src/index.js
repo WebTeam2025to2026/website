82014b3f55b9d694edf2f4fe30fcf2f680fb9835
Sanjaykishore T <sanjaykishore1816@gmail.com>
2025-10-02 09:24:05 +0530

    test

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
-<!-- public/index.html -->
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="theme-color" content="#000000" />
-    <meta name="description" content="Quizzards - Quiz Club of REC" />
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-    <!-- Google Search Console Verification -->
-    <meta name="google-site-verification" content="PpfTxVPrPPPhq9nWhd6H0rQLOGU1EtStqQzyiDV4Rks" />
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-    <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
-    <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
-    <title>Quizzards REC</title>
-  </head>
-  <body>
-    <noscript>You need to enable JavaScript to run this app.</noscript>
-    <div id="root"></div>
-  </body>
-</html>
+reportWebVitals(console.log);
